718b0bd5e77e1cef7f8760b2ae1c1fe11d378d53
GuyKroizman <kroizguy@gmail.com>
2016-01-29 18:12:52 +0200

    make camera follow shepard

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Shepard;
+
+    private Vector3 offset;
+
+	void Start () {
+        offset = transform.position - Shepard.transform.position;
+	}
+	
+
+	void LateUpdate () {
+        transform.position = Shepard.transform.position + offset;
+	}
+}
