1782e4219bd85132425bb6e5788df959a4fbcadc
GuyKroizman <kroizguy@gmail.com>
2016-01-29 22:50:16 +0200

    sheeps following

---
+++ Assets/SheepController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepController : MonoBehaviour {
+
+    public Rigidbody Shepard;
+
+    public int moveSpeed;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 directionTowardsPlayer = (Shepard.position - transform.position).normalized;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(directionTowardsPlayer * moveSpeed * Time.deltaTime);
+    }
+}
