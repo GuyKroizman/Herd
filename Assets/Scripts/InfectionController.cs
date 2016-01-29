4a04558f7a7a8260ecc2c027ba5e409d6adf1071
GuyKroizman <kroizguy@gmail.com>
2016-01-30 00:25:44 +0200

    add infector and color changing for sheeps

---
+++ Assets/Scripts/InfectionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfectionController : MonoBehaviour {
+
+    public Rigidbody target;
+
+    public int moveSpeed;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 directionTowardsPlayer = (target.position - transform.position).normalized;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(directionTowardsPlayer * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SheepController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheepController : MonoBehaviour {
+
+    public Rigidbody Shepard;
+
+    public int moveSpeed;
+
+    public float sickTendancy;
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 directionTowardsPlayer = (Shepard.position - transform.position).normalized;
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(directionTowardsPlayer * moveSpeed * Time.deltaTime);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.name == "Infection1")
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            Color jeff = new Color(renderer.material.color.r - sickTendancy, 1f, renderer.material.color.b - sickTendancy);
+            renderer.material.color = jeff;
+        }
+    }
+}
