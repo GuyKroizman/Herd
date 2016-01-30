01d695f6a083251621241dfe22dc9c242b126033
Yogev Levy <MrYogevLevy@gmail.com>
2016-01-30 18:56:47 +0200

    adding sounds

--- Assets/Scripts/SheepController.cs
+++ Assets/Scripts/SheepController.cs
@@ -10,12 +10,33 @@ public class SheepController : MonoBehaviour {
 
     public float sickTendancy;
 
+	public AudioSource cough1;
+	public AudioSource cough2;
+
+	public AudioSource abba1;
+	public AudioSource abba2;
+
+
+	private int intervals = 0;
+
     // Update is called once per frame
     void Update () {
         Vector3 directionTowardsPlayer = (Shepard.position - transform.position).normalized;
 
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(directionTowardsPlayer * moveSpeed * Time.deltaTime);
+
+
+		intervals++;
+		if (intervals % 100 == 0) {
+			if (UnityEngine.Random.value > 0.85) {
+				if (UnityEngine.Random.value > 0.5) 
+					abba1.Play();
+				else
+					abba2.Play();
+			}
+
+		}
     }
 
     void OnCollisionEnter(Collision collision)
@@ -26,6 +47,12 @@ public class SheepController : MonoBehaviour {
 			float newColorFloat = Math.Max(0f, renderer.material.color.r - sickTendancy);
 			Color jeff = new Color(newColorFloat, 1f, newColorFloat);
             renderer.material.color = jeff;
+
+			if (UnityEngine.Random.value > 0.5) 
+				cough1.Play();
+			else
+				cough2.Play();
+
         }
     }
 }
