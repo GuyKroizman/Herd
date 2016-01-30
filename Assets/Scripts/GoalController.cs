e119149547da96acde7812d20c0fea084397f034
GuyKroizman <kroizguy@gmail.com>
2016-01-30 03:10:50 +0200

    add goal plane

---
+++ Assets/Scripts/GoalController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalController : MonoBehaviour {
+    
+    private bool sheep1Arrived = false;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Sheep1")
+        {
+            sheep1Arrived = true;
+        }
+
+   
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+
+    }
+}
