72d824225f4b756cf56956478f4464df8e6fab1a
GuyKroizman <kroizguy@gmail.com>
2016-02-01 01:21:31 +0200

    add exit by escape key

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.LoadLevel("GameMenu");
+        }
+    }
+}
